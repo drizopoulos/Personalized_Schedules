b5ba74e559f0cd0de91831c285da1224c91837da
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2020-01-06 20:03:04 +0100

    updates

---
+++ 2020_NKI/copy_files.R
@@ -0,0 +1,17 @@
+rmarkdown::render_site(input = "./2020_NKI", encoding = 'UTF-8')
+
+
+dirs <- list.dirs(path = "./2020_NKI/_site/", full.names = FALSE, recursive = TRUE)
+files <- list.files(path = "./2020_NKI/_site/", include.dirs = TRUE, recursive = TRUE)
+
+for (i in seq_along(dirs)) {
+    dir.create(path = paste0("./docs/", dirs[i]), showWarnings = FALSE)
+}
+
+for (i in seq_along(files)) {
+    file.copy(from = paste0("./2020_NKI/_site/", files[i]), 
+              to = paste0("./docs/", files[i]), overwrite = TRUE)
+}
+
+
+
