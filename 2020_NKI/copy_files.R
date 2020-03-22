536e78292522e924ea4df309f7c9369af3d6aa4e
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2020-03-22 20:21:04 +0100

    ENAR slides

--- 2020_NKI/copy_files.R
+++ 2020_NKI/copy_files.R
@@ -1,15 +1,15 @@
-rmarkdown::render_site(input = "./2020_NKI", encoding = 'UTF-8')
+rmarkdown::render_site(input = "./2020_ENAR", encoding = 'UTF-8')
 
 
-dirs <- list.dirs(path = "./2020_NKI/_site/", full.names = FALSE, recursive = TRUE)
-files <- list.files(path = "./2020_NKI/_site/", include.dirs = TRUE, recursive = TRUE)
+dirs <- list.dirs(path = "./2020_ENAR/_site/", full.names = FALSE, recursive = TRUE)
+files <- list.files(path = "./2020_ENAR/_site/", include.dirs = TRUE, recursive = TRUE)
 
 for (i in seq_along(dirs)) {
     dir.create(path = paste0("./docs/", dirs[i]), showWarnings = FALSE)
 }
 
 for (i in seq_along(files)) {
-    file.copy(from = paste0("./2020_NKI/_site/", files[i]), 
+    file.copy(from = paste0("./2020_ENAR/_site/", files[i]), 
               to = paste0("./docs/", files[i]), overwrite = TRUE)
 }
 
