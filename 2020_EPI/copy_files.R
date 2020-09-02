672af89ac811a4dd45a0d820f86fc511f1c55e30
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2020-09-02 21:24:13 +0200

    EPI presentation

---
+++ 2020_EPI/copy_files.R
@@ -0,0 +1,17 @@
+rmarkdown::render_site(input = "./2020_EPI", encoding = 'UTF-8')
+
+
+dirs <- list.dirs(path = "./2020_EPI/_site/", full.names = FALSE, recursive = TRUE)
+files <- list.files(path = "./2020_EPI/_site/", include.dirs = TRUE, recursive = TRUE)
+
+for (i in seq_along(dirs)) {
+    dir.create(path = paste0("./docs/", dirs[i]), showWarnings = FALSE)
+}
+
+for (i in seq_along(files)) {
+    file.copy(from = paste0("./2020_EPI/_site/", files[i]), 
+              to = paste0("./docs/", files[i]), overwrite = TRUE)
+}
+
+
+
