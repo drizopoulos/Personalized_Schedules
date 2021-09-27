912d806e8c7ef4be40ae8a809015fbedc7d79be0
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2021-09-27 13:48:25 +0200

    UMich

---
+++ 2021_Michigan/copy_files.R
@@ -0,0 +1,17 @@
+rmarkdown::render_site(input = "./2021_Michigan", encoding = 'UTF-8')
+
+
+dirs <- list.dirs(path = "./2021_Michigan/_site/", full.names = FALSE, recursive = TRUE)
+files <- list.files(path = "./2021_Michigan/_site/", include.dirs = TRUE, recursive = TRUE)
+
+for (i in seq_along(dirs)) {
+    dir.create(path = paste0("./docs/", dirs[i]), showWarnings = FALSE)
+}
+
+for (i in seq_along(files)) {
+    file.copy(from = paste0("./2021_Michigan/_site/", files[i]), 
+              to = paste0("./docs/", files[i]), overwrite = TRUE)
+}
+
+
+
